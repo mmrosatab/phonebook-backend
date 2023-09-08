bc9772dd1aa5dc07426e26555a4b50ec2dba5f15
mmrosatab <mmrosatab@hotmail.com>
2023-09-07 22:05:42 -0300

    Feature: Add post route

--- index.js
+++ index.js
@@ -14,9 +14,37 @@ app.get("/contacts", (req, res) => {
     fs.readFile(PATH, 'utf8', (err, data) => {
         if (err) {
           console.error(err)
-          return res.status(500).send('Erro ao ler os dados')
+          return res.status(500).send('Error reading data')
         }
-        res.json(JSON.parse(data))
+        return res.json(JSON.parse(data))
+    })
+})
+
+app.post('/contacts', (req, res) => {
+    fs.readFile(PATH, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err)
+        return res.status(500).send('Error reading data')
+      }
+  
+      const contacts = JSON.parse(data)
+      const newContact = req.body
+      const { id } = newContact
+      const hasContact = contacts.find(contact => contact.id === id)
+
+      if(!!hasContact){
+        return res.status(409).send({message: 'Contact alredy exits'})
+      }
+
+      contacts.push(newContact)
+
+      fs.writeFile(PATH, JSON.stringify(contacts), (err) => {
+        if (err) {
+          console.error(err)
+          return res.status(500).send('Error saving contacts')
+        }
+        return res.status(200).send(JSON.stringify(newContact))
+      })
     })
 })
 
